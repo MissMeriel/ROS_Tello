c62e125f447203ce13bd6f9c771ce4e90113ec9f
meriel <meriel.k.stein@gmail.com>
2019-03-21 18:40:17 -0400

    CAMERA IS WORKING

--- src/dronet_tello/scripts/camera.py
+++ src/dronet_tello/scripts/camera.py
@@ -11,61 +11,100 @@ import cv2
 import imageio
 import numpy as np
 import rospy
+
+#import libh264decoder
+
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
 from tellopy import build_packet, TelloCommand
 
+#decoder = libh264decoder.H264Decoder()
 
 def stream_video(output_stream, shutdown_signal):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    addrVideo = ("", 6037)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.settimeout(.5)
+    addrVideo = ('', 6037)
+    #addrVideo = ('', 11111)
     sock.bind(addrVideo)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #s.settimeout(1)
     addr = ("192.168.10.1", 8889)
+    if(sock is None):
+	print("Could not connect to local video socket")
+    if(s is None):
+	print("Could not connect to Tello video socket")
     print("CONNECTING TO CAMERA...")
-    s.sendto(bytearray(b"conn_req:" + struct.pack("<H", 6037)), addr)
-    time.sleep(2.0)
-    # print("TAKING OFF...")
-    # s.sendto(build_packet(TelloCommand.TAKEOFF, sequence_id=1), addr)
-    s.close()
+    bytes_sent = s.sendto(bytearray(b"conn_req:" + struct.pack("<H", 6037)), addr)
+    print("bytes sent: "+str(bytes_sent))
+    #s.sendto(bytearray(b"conn_req:" + struct.pack("<H", 11111)), addr)
+    #time.sleep(2.0)
+    print("TAKING OFF...")
+    s.sendto(build_packet(TelloCommand.TAKEOFF, sequence_id=1), addr)
+    print("REQUESTING VIDEO...")
+    bytes_sent = s.sendto(build_packet(TelloCommand.REQ_VIDEO_SPS_PPS, sequence_id=1), addr)
+    print("bytes sent: "+str(bytes_sent))
+    #s.sendto(b'command', addr)
+    #print ('sent: command')
+    #s.sendto(b'streamon', addr)
+    #print ('sent: streamon')
+    #s.close()
 
     # with open("debug.h264", "w+") as capture_file:
     #     pass
 
     is_started = False
+    packet_data = ""
     while not shutdown_signal.is_set():
         try:
-            data = bytearray(sock.recv(4096))
+                #res_string, ip = sock.recvfrom(2048)
+                res_string = sock.recv(4096)
+                packet_data += res_string
+                # end of frame
+                if len(res_string) != 1460:
+                    print("time to decode")
+                    packet_data = ""
+
+		data = bytearray(res_string)
         except socket.timeout as e:
+            print(e)
+	    print("REQUESTING VIDEO...")
+	    bytes_sent = s.sendto(build_packet(TelloCommand.REQ_VIDEO_SPS_PPS, sequence_id=1), addr)
             continue
         except socket.error as e:
             print(e)
             break
         if len(data) > 6 and data[2:6] == b"\x00\x00\x00\x01" and data[6] & 0x1f == 7:
             # sequence parameter set has been received, we can start saving
-            is_started = True
-        if is_started:
+        	is_started = True
+        if True:
             # with open("debug.h264", "ab") as capture_file:
             #     capture_file.write(data[2:])
+	    print("writing to output_stream...")
             output_stream.write(data[2:])
+	print("is_started: "+str(is_started))
+	print("data[2:6] == b\"\x00\x00\x00\x01\": "+str(data[2:6] == b"\x00\x00\x00\x01"))
+	print(data[2:6])
+	print("data[6] & 0x1f == 7: "+str(data[6] & 0x1f == 7))
+	print
     sock.close()
 
 
 def decode_bmp_image(input_stream, shutdown_signal):
+    print("in decode_bmp_image")
     camera_raw = rospy.Publisher("image_raw", Image, queue_size=10)
     cnt = 0
     rootpath = "capture"
     if not os.path.exists(rootpath):
         os.makedirs(rootpath)
     while not shutdown_signal.is_set():
+	print("reading from input stream...")
         file_size_bytes = bytearray(input_stream.read(6))
-        # print("IMAGE PROCESSED!")
-        # print(" size: %s" % file_size_bytes)
+        print("IMAGE PROCESSED!")
+        print(" size: %s" % file_size_bytes)
         file_size = 0
         for i in range(4):
             file_size += file_size_bytes[i + 2] * 256 ** i
@@ -73,7 +112,7 @@ def decode_bmp_image(input_stream, shutdown_signal):
         image = cv2.imdecode(np.fromstring(bytes(data), dtype=np.uint8), 1)
         temp = rospy.Time.now()
         cv2.imwrite("{}/{}.jpg".format(rootpath, temp), image)
-        # print(" decoded!")
+        print(" decoded!")
         # image = np.roll(image, 2, axis=-1)
         image_msg = CvBridge().cv2_to_imgmsg(image)
         image_msg.header.stamp = rospy.Time.now()
@@ -100,6 +139,7 @@ def main():
         # "fps=2",
         "-",
     ]
+    print(sys.version)
     print(" ".join(ffmpegCmd))
     with open("ffmpeg.err", "w+") as ffmpeg_err:
         ffmpeg = sp.Popen(ffmpegCmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=ffmpeg_err)
